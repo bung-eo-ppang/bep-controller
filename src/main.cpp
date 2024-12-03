def475ed092104bc21a88cb8347669539bf5b64f
2paperstar <lee@paperst.ar>
2024-12-03 10:36:22 +0900

    feat: controller code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,90 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <printf.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TRANSMITTER
+
+RF24 radio(7, 8);
+const byte address[6] = "00001";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("testtest");
+  if (!radio.begin()) {
+    Serial.println("Radio hardware is not responding.");
+    while (1);
+  }
+
+  radio.setPALevel(RF24_PA_MIN);
+#ifdef TRANSMITTER
+  radio.openWritingPipe(address);
+  radio.stopListening();
+#else
+  radio.openReadingPipe(0, address);
+  radio.startListening();
+#endif
+  printf_begin();
+  radio.printDetails();
+  radio.printPrettyDetails();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+#ifdef TRANSMITTER
+  const char* text = "Hello, World!";
+  Serial.println(radio.write(text, strlen(text)));
+  delay(1000);
+#else
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(text, sizeof(text));
+    Serial.println(text);
+  }
+#endif
+}
+#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <printf.h>
+
+#define TRANSMITTER
+
+RF24 radio(7, 8);
+const byte address[6] = "00001";
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("testtest");
+  if (!radio.begin()) {
+    Serial.println("Radio hardware is not responding.");
+    while (1);
+  }
+
+  radio.setPALevel(RF24_PA_MIN);
+#ifdef TRANSMITTER
+  radio.openWritingPipe(address);
+  radio.stopListening();
+#else
+  radio.openReadingPipe(0, address);
+  radio.startListening();
+#endif
+  printf_begin();
+  radio.printDetails();
+  radio.printPrettyDetails();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() {
+#ifdef TRANSMITTER
+  const char* text = "Hello, World!";
+  Serial.println(radio.write(text, strlen(text)));
+  delay(1000);
+#else
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(text, sizeof(text));
+    Serial.println(text);
+  }
+#endif
 }
