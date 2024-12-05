596f8159ccfd241d7e895c858d605a134b96943c
2paperstar <lee@paperst.ar>
2024-12-05 10:28:40 +0900

    chore: mpu6050 test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,43 @@
 #include <Arduino.h>
+#include <I2Cdev.h>
+#include <MPU6050_6Axis_MotionApps20.h>
 #include "transmitter.hpp"
 
 const byte address[6] = "00001";
+MPU6050 mpu;
 Transmitter transmitter(address);
 
+// #define INTERRUPT_PIN 4
+
 void setup() {
-  Serial.begin(9600);
-  transmitter.init();
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  // pinMode(INTERRUPT_PIN, INPUT);
+
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  Serial.println(F("Initializing DMP..."));
+  // uint8_t devStatus = mpu.dmpInitialize();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+
+  // transmitter.init();
 }
 
 void loop() {
-  transmitter.write();
+  // Serial.println("Hello, World!");
+
+  // transmitter.write();
 }
