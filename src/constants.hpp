ca88da73decf06425dd03055980efd29a9f1ca33
2paperstar <lee@paperst.ar>
2024-12-07 00:57:54 +0900

    feat: protocol

---
+++ src/constants.hpp
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include <Arduino.h>
+
+
+const uint8_t SIGNATURE[] = { 0x94, 'B', 'E', 'P', 0x0d, 0x0a, 0x1a, 0x0a };
+const uint8_t VERSION[] = { 0x00, 0x00 };
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "transmitter.hpp"
 #include "gyro.hpp"
+#include "constants.hpp"
 
 const byte address[6] = "00001";
 Transmitter transmitter(address);
@@ -15,9 +16,30 @@ void setup() {
 }
 
 void loop() {
-  // Serial.println("Hello, World!");
-
   // transmitter.write();
   auto ypr = gyro.getYawPitchRoll();
-  Serial.println("yaw: " + String(ypr.yaw) + " pitch: " + String(ypr.pitch) + " roll: " + String(ypr.roll));
+  uint32_t time = millis();
+  int count = 1;
+
+  Serial.write(SIGNATURE, sizeof(SIGNATURE));
+  Serial.write((byte*)&time, sizeof(time));
+  Serial.write((byte*)&count, sizeof(count));
+  Serial.write(VERSION, sizeof(VERSION));
+
+  for (short i = 0; i < count; i++) {
+    Serial.write((byte*)&i, sizeof(i));
+    Serial.write(VERSION, sizeof(VERSION));
+    Serial.write((byte*)&time, sizeof(time));
+
+    Serial.write((byte*)&ypr.yaw, sizeof(ypr.yaw));
+    Serial.write((byte*)&ypr.pitch, sizeof(ypr.pitch));
+    Serial.write((byte*)&ypr.roll, sizeof(ypr.roll));
+
+    float joyX = 0;
+    float joyY = 0;
+    byte buttons[] = { 0b1111, 0b1111, 0b1111, 0b1111 };
+    Serial.write((byte*)&joyX, sizeof(joyX));
+    Serial.write((byte*)&joyY, sizeof(joyY));
+    Serial.write((byte*)&buttons, sizeof(buttons));
+  }
 }
