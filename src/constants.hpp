2e78d19941b32699b0183645c70c0be393e16955
2paperstar <lee@paperst.ar>
2024-12-10 09:22:30 +0900

    chore: upgrade version

--- src/constants.hpp
+++ src/constants.hpp
@@ -5,6 +5,6 @@
 
 
 const uint8_t SIGNATURE[] = { 0x94, 'B', 'E', 'P', 0x0d, 0x0a, 0x1a, 0x0a };
-const uint16_t VERSION = 1;
+const uint16_t VERSION = 2;
 const uint8_t END_SIGNATURE[] = { 0x95, 'B', 'E', 'P', 0x0d, 0x0a, 0x1a, 0x0a };
 #endif
